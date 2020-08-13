139dfc26daeca74b1db0c7c7a0a699c8176f8cc2
kevinzhangnz <51142004+kevinzhangnz@users.noreply.github.com>
2020-08-13 23:07:49 +1200

    create home component as landing page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'posts',
+    component: AppComponent,
+    loadChildren: () => import('./modules/posts/posts.module').then(m => m.PostsModule)
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
